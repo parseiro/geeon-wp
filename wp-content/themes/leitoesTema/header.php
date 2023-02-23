793eec555ee5f8e3849c4e0de7e609d060f67eb5
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-02-23 15:51:08 -0300

    Mudancas de tema, dark mode

--- wp-content/themes/leitoesTema/header.php
+++ wp-content/themes/leitoesTema/header.php
@@ -18,10 +18,10 @@
     <link rel="apple-touch-icon" href="icon.png"/>
     <!-- Place favicon.ico in the root directory -->
 
-    <link
+<!--    <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
-    />
+    />-->
     <!--    <link rel="stylesheet" href="css/normalize.css" />-->
     <!--    <link rel="stylesheet" href="css/main.css" />-->
     <!--    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.6.0/dist/flowbite.min.css"/>-->
@@ -33,15 +33,45 @@
 	<?php wp_head(); ?>
 </head>
 
-<body class="container mx-auto flex max-w-4xl flex-col scroll-smooth border-solid border-gray-100 justify-between">
+<body>
 <header>
     <nav class="border-gray-200 bg-white px-4 py-2.5 dark:bg-gray-800 lg:px-6">
         <div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-            <a href="https://flowbite.com" class="flex items-center">
-                <img src="../../../content/images/iconGeeon.svg" class="mr-3 h-6 sm:h-9" alt="GEEON Logo" />
+            <div class="flex items-center">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/iconGeeon.svg" class="mr-3 h-6 sm:h-9" alt="GEEON Logo" />
                 <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">GEEON</span>
-            </a>
+            </div>
             <div class="flex items-center lg:order-2">
+                <button
+                        id="theme-toggle"
+                        type="button"
+                        class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+                >
+                    <svg
+                            id="theme-toggle-dark-icon"
+                            class="w-5 h-5 hidden"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                                d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+                        ></path>
+                    </svg>
+                    <svg
+                            id="theme-toggle-light-icon"
+                            class="w-5 h-5 hidden"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                fill-rule="evenodd"
+                                clip-rule="evenodd"
+                        ></path>
+                    </svg>
+                </button>
                 <a
                         href="#"
                         class="mr-2 rounded-lg px-4 py-2 text-sm font-medium text-gray-800 hover:bg-gray-50 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800 lg:px-5 lg:py-2.5"
@@ -247,7 +277,7 @@
                     </li>
                     <li>
                         <a
-                                href="#"
+                                href="contato"
                                 class="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:border-0 lg:p-0 lg:hover:bg-transparent lg:hover:text-primary-600 lg:dark:hover:bg-transparent lg:dark:hover:text-primary-500"
                         >
                             Contato
