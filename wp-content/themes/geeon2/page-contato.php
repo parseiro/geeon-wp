f1f742ae045197b092376a1dc07b7893cb792cb4
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-02-24 11:59:52 -0300

    Partial layout

--- wp-content/themes/geeon2/page-contato.php
+++ wp-content/themes/geeon2/page-contato.php
@@ -3,34 +3,59 @@
 ?>
 <?php get_header(); ?>
 
-	<section class="bg-white dark:bg-gray-900">
-		<div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-	<h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"><?php the_title(); ?></h2>
-	<p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl"><?php the_content(); ?></p>
-
-<?php endwhile; else: endif; ?>
-<!--		<form action="#" class="space-y-8">
-            <div>
-                <label for="email" class="name-input">Your name</label>
-                <input type="email" id="email" class="email-input" placeholder="name@flowbite.com" required>
-            </div>			<div>
-				<label for="email" class="email-input">Your email</label>
-				<input type="email" id="email" class="email-input" placeholder="name@flowbite.com" required>
-			</div>
-			<div>
-				<label for="subject" class="assunto-input">Subject</label>
-				<input type="text" id="subject" class="subject-input" placeholder="Let us know how we can help you" required>
-			</div>
-			<div class="sm:col-span-2">
-				<label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-				<textarea id="message" rows="6" class="message-textarea" placeholder="Leave a comment..."></textarea>
-			</div>
-			<button type="submit" class="submit-button">Send message</button>
-		</form>-->
-	</div>
-</section>
+    <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <div class="px-4 mx-auto max-w-screen-sm text-center lg:px-6 mb-8 lg:mb-16">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Contato</h2>
+                <p class="font-light text-gray-600 dark:text-gray-400 sm:text-xl">We use an agile approach to test
+                    assumptions and connect with the needs of your audience early and often.</p>
+            </div>
+            <div class="grid grid-cols-1 col-span-1 gap-8 text-center sm:grid-cols-2 lg:grid-cols-3">
+                <div>
+                    <div class="flex justify-center items-center mx-auto mb-4 w-10 h-10 bg-gray-100 rounded-lg dark:bg-gray-800 lg:h-16 lg:w-16">
+                        <svg class="w-5 h-5 text-gray-600 lg:w-8 lg:h-8 dark:text-gray-500" fill="currentColor"
+                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z"
+                                  clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <p class="mb-2 text-xl font-bold text-gray-900 dark:text-white">Company information:</p>
+                    <p class="text-gray-500 dark:text-gray-400">Themesberg LLC <br>Tax id: USXXXXXX</p>
+                </div>
+                <div>
+                    <div class="flex justify-center items-center mx-auto mb-4 w-10 h-10 bg-gray-100 rounded-lg dark:bg-gray-800 lg:h-16 lg:w-16">
+                        <svg class="w-5 h-5 text-gray-600 lg:w-8 lg:h-8 dark:text-gray-500" fill="currentColor"
+                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                                  clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <p class="mb-2 text-xl font-bold text-gray-900 dark:text-white">Address:</p>
+                    <p class="text-gray-500 dark:text-gray-400">SILVER LAKE, United States 1941 Late Avenue <br> Zip
+                        Code/Postal code:03875</p>
+                </div>
+                <div>
+                    <div class="flex justify-center items-center mx-auto mb-4 w-10 h-10 bg-gray-100 rounded-lg dark:bg-gray-800 lg:h-16 lg:w-16">
+                        <svg class="w-5 h-5 text-gray-600 lg:w-8 lg:h-8 dark:text-gray-500" fill="currentColor"
+                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+                        </svg>
+                    </div>
+                    <p class="mb-2 text-xl font-bold text-gray-900 dark:text-white">Call us:</p>
+                    <p class="mb-3 text-gray-500 dark:text-gray-400">Call us to speak to a member of our team. We
+                        are
+                        always happy to help.</p>
+                    <p class="font-semibold text-primary-600 dark:text-primary-500">+1 (646) 786-5060</p>
+                </div>
+            </div>
+            <div class="mb-8 lg:mb-0">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; else: endif; ?>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
